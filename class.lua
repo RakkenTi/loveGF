de84388d27b913eae78a54d733efc3aa9d76fd81
RakkenTi <martin3zmrk@gmail.com>
2025-04-09 10:27:47 -0400

    fixed class.lua :extend() implementation, indexed self for proper OOP

--- class.lua
+++ class.lua
@@ -1,15 +1,18 @@
 --- Create a new class.
 --- @param class table? The class to inherit. Providing no class creates a blank one.
+--- @return baseClass
 return function(class)
 
     class = class or {}
     class.__index = class
 
     --- @class baseClass
+    --- @field extend fun(self: table):baseClass Extend the current class and create a subset class that references the old class. Returns a subset of the class calling the function.
+    --- @field implement fun(self: table, classToImplement: table):self Implement the class passed in as a parameter. Copies methods from classToImplement to the current class.
+    --- Does not overwrite already existing methods.
     local self = setmetatable({}, class)
+    self.__index = self
 
-    --- Extend the current class and create a subset class that references the old class.
-    --- @return table Returns a subset of the class.
     function class:extend()
         local subclass = {}
         subclass.__index = subclass
@@ -17,9 +20,6 @@ return function(class)
         return subclass
     end
 
-    --- Implement the class passed in. Basically copies all methods from said class into the current class.
-    --- Does not overwrite already existing methods.
-    --- @param classToImplement table? The class to implement.
     function class:implement(classToImplement)
         for k, v in pairs(classToImplement) do
             if not self[k] then
