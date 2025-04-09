899356b314695107a3908ec557fc174fb92f6644
RakkenTi <martin3zmrk@gmail.com>
2025-04-09 17:26:33 -0400

    added children func

--- loveUI/base/drawableUIInterface.lua
+++ loveUI/base/drawableUIInterface.lua
@@ -1,24 +1,30 @@
 --- Base class for any drawable UI.
 --- @class drawableUIInterface : baseClass
---- @field children table
 local drawableUIBaseClass = relrequire("...class"):extend()
+--- @type drawableUIInterface[]
+drawableUIBaseClass.children = {}
 
 -- Schedulers
 function drawableUIBaseClass:draw()
-    error(self.__name .. ": Draw function is not implemented!")
+    error(": Draw function is not implemented!")
 end
 
 function drawableUIBaseClass:update()
-    error(self.__name ..  ": Update function is not implemented!")
+    error(": Update function is not implemented!")
 end
 
 function drawableUIBaseClass:resize()
-    error(self.__name .. ": Resize function is not implemented!")
+    error(": Resize function is not implemented!")
 end
 
 -- Base
 function drawableUIBaseClass:setAlpha()
-    error(self.__name ..  ": setAlpha function is not implemented!")
+    error(": setAlpha function is not implemented!")
+end
+
+--- @param child drawableUIInterface
+function drawableUIBaseClass:addChild(child)
+    table.insert(self.children, child)
 end
 
 return drawableUIBaseClass
